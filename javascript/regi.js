588baf2719b2e3771acbdeb425294a7ac58062f2
A3-403-02 <naphadesahil@gmail.com>
2019-08-27 13:16:28 +0530

    Added the password checking validation.

--- javascript/regi.js
+++ javascript/regi.js
@@ -1,8 +1,14 @@
-var valid_pass = function() {
-    if (document.getElementById('pswd').value == document.getElementById('cnfpswd').value) {
+function valid_pass() {
+    var v1 = document.forms["registernew"]["pswd"].value;
+    var v2 = document.forms["registernew"]["cnfpswd"].value;
+    console.log("V1 is "+v1);
+    console.log("V2 is "+v2);
+    if (v1 == v2) {
+      console.log("Equal");
       document.getElementById('not-equal').style.color = 'green';
       document.getElementById('not-equal').innerHTML = 'matching';
     } else {
+      console.log("Not equal")
       document.getElementById('not-equal').style.color = 'red';
       document.getElementById('not-equal').innerHTML = 'not matching';
     }
